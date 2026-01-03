b1a35a2280b64d500aa024be4655520a2a179805
bharathkusuma123 <bharathkusuma4953@gmail.com>
2026-01-03 10:37:34 +0530

    bharath

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WebHome from "./WebHome/WebHome";
 import WebsiteNavbar from "./WebsiteNavbar/WebsiteNavbar";
 import SubCategories from "./SubCategories/SubCategories";
+import ProductDetails from "./ProductDetails/ProductDetails"
+import Filters from "./Main/Filters";
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
         <Route path="/" element={<WebsiteNavbar />} />
         <Route path="/webhome" element={<WebHome />} />
         <Route path="/category/:id" element={<SubCategories />} />
+        <Route path="/filters" element={<SubCategories />} />
+<Route path="/w-productdetails" element={<ProductDetails/>} />
+<Route path="/w-category/:id" element={<SubCategories />} />
       </Routes>
     </BrowserRouter>
   );
