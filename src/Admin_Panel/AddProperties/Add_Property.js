6e78889319e9bd837b7df48f95d6aec71c95dabd
kodamharish <kodamharish12@gmail.com>
2026-01-13 17:59:50 +0530

    updated add property file

--- src/Admin_Panel/AddProperties/Add_Property.js
+++ src/Admin_Panel/AddProperties/Add_Property.js
@@ -572,7 +572,7 @@ const AssetForm = ({ user, mode = 'add' }) => {
             />
             <button 
               type="button"
-              className="btn btn-outline-secondary"
+              className="btn btn-outline-secondary media-upload-btn"
               onClick={() => document.getElementById(`${type}-upload`).click()}
               disabled={isViewing}
             >
